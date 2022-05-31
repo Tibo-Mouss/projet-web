6dc58d57be8693cc15b1c8e7cee2f5d8a65b38df
Tibo Mousset <83610454+Tibo-Mouss@users.noreply.github.com>
2022-06-01 01:20:01 +0200

    test

---
+++ src/main/webapp/test.java
@@ -0,0 +1,9 @@
+import org.jboss.as.FacadeTheComeback;
+import org.jboss.as.quickstarts.helloworld.MainServlet;
+
+
+public class test {
+    public void testage() {
+        FacadeTheComeback teso = MainServlet.facade;
+    }
+}
