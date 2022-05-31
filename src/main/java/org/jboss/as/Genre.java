396a946652f8074d3d2631bdb690c83872214412
Alexandre Koch <alexandrekoch3@gmail.com>
2022-05-31 23:05:00 +0200

    genres en plus

--- src/main/java/org/jboss/as/Genre.java
+++ src/main/java/org/jboss/as/Genre.java
@@ -1,3 +1,3 @@
 package org.jboss.as;
 
-public enum Genre {Shonen, Seinen, Shojo, Ecchi, Action, Aventure, Comedie, Drame, Fantasy}
+public enum Genre {Shonen, Seinen, Shojo, Ecchi, Action, Aventure, Comedie, Drame, Fantasy, Horreur, Isekai, Romance, School_Life, Slice_Of_Life}
