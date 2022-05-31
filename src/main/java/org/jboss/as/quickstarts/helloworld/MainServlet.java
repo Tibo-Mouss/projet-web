167503915da231145731286b73073e8ee9d785df
Tibo Mousset <83610454+Tibo-Mouss@users.noreply.github.com>
2022-06-01 01:29:51 +0200

    bon ba decevant hein

--- src/main/java/org/jboss/as/quickstarts/helloworld/MainServlet.java
+++ src/main/java/org/jboss/as/quickstarts/helloworld/MainServlet.java
@@ -21,7 +21,7 @@ public class MainServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	@EJB
-	public static FacadeTheComeback facade;
+	private FacadeTheComeback facade;
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -93,6 +93,7 @@ public class MainServlet extends HttpServlet {
 				}
 
 				if (operation_succeeded) {
+					request.setAttribute("facade", facade);
 					RequestDispatcher disp = request.getRequestDispatcher("home.jsp");
 					disp.forward(request,response);
 				} else if (op.equals("login")) {
