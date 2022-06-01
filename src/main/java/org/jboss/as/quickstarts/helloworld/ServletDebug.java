acc20ed597422148996521b898d4c728e1db6d30
Alexandre Koch <alexandrekoch3@gmail.com>
2022-06-01 13:27:14 +0200

    debug servlet fait avec BDD

--- src/main/java/org/jboss/as/quickstarts/helloworld/ServletDebug.java
+++ src/main/java/org/jboss/as/quickstarts/helloworld/ServletDebug.java
@@ -1,13 +1,22 @@
 package org.jboss.as.quickstarts.helloworld;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.ejb.EJB;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.jboss.as.FacadeTheComeback;
+import org.jboss.as.Genre;
+import org.jboss.as.Manga;
 
 
 /**
@@ -16,19 +25,40 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/ServletDebug")
 public class ServletDebug extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+    
+
+	@EJB
+	private FacadeTheComeback facade;
+	
     /**
      * @see HttpServlet#HttpServlet()
      */
     public ServletDebug() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		List<Manga> mangas = facade.getMangas();
+		Manga mg1 = new Manga();
+		mg1.setAuthor("Oda Eiichiro");
+		mg1.setTitle("One Piece");
+		mg1.setDescription("Gloire, fortune et puissance, c'est ce que possédait Gold Roger, le tout puissant roi des pirates, avant de mourir sur l'échafaud. Mais ses dernières paroles ont éveillées bien des convoitises, et lança la fabuleuse 'ère de la piraterie', chacun voulant trouver le fabuleux trésor qu'il disait avoir laissé. Bien des années plus tard, Shanks, un redoutable pirate aux cheveux rouges, rencontre Luffy, un jeune garçon d'une dizaine d'années dans un petit port de pêche. Il veut devenir pirate et le rejoindre, mais Shanks lui répond qu'il est trop jeune. Plus tard, Luffy avalera accidentellement le fruit Gomu Gomu qui rendra son corps élastique, mais aussi maudit par les eaux. Incapable de nager, Luffy ne veut pourtant pas renoncer à son rêve. Pour le consoler lorsqu'il part, Shanks lui offre son chapeau. Luffy jure alors de le rejoindre un jour avec son propre équipage. A 17 ans, Luffy prend la mer dans une petite barque avec pour but de réunir un équipage de pirates, mais de pirates pas comme les autres, qui devront partager sa conception un peu étrange de la piraterie. L'aventure est lancée.");
+		List<Genre> genres = new ArrayList();
+		genres.add(Genre.Shonen);
+		genres.add(Genre.Aventure);
+		mg1.setGenres(genres);
+		List<URL> chapters = new ArrayList<>();
+		try {
+			chapters.add(new URL("https://drive.google.com/uc?export=download&id=11pNtUJECkMpHSmXzqIaMjW3VATnhuXIP"));
+			mg1.setCover(new URL("https://drive.google.com/uc?export=download&id=1fGDVTXVdp9PjfuxhCpQ201KFTR6VWzDv"));
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+		mg1.setChapters(chapters);
 		
 		
 		/* Exemple calculatrice 
@@ -39,12 +69,8 @@ public class ServletDebug extends HttpServlet {
 		*/
 
 
-		String op = request.getParameter("op");
+		response.getWriter().println("<html><body> La base virale VPN a été mise à jour </body></html>");
 
-		switch(op) {
-			
-		}
-		
 	}
 
 	/**
